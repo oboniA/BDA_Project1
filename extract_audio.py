e2908ba08ea8e331d343a9a5ade93a331db2dbd1
Oboni <oanowe01@student.bbk.ac.uk>
2024-06-10 17:04:45 +0100

    Task 5 Extract Audio from Video file and save all audios in a folder

---
+++ extract_audio.py
@@ -0,0 +1,26 @@
+import moviepy.editor as mp
+import os
+
+
+def audio_extraction(video_location, audio_location):
+
+    # directory will be created for audios
+    os.makedirs(audio_location, exist_ok=True)
+
+    # loops through the video volder
+    for title in os.listdir(video_location):
+         
+        # constructs path with folder name and file name
+        video_path = os.path.join(video_location, title)
+
+        # constructs output file path
+        out_filename= os.path.splitext(title)[0] + ".wav"
+        out_path= os.path.join(audio_location, out_filename)
+
+        # extracts audio from video
+        video= mp.VideoFileClip(video_path)
+        # pcm_s16le foir 16-bit WAV output
+        video.audio.write_audiofile(out_path, codec='pcm_s16le')
+
+        print(f'extraction complete')
+
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 # Main File
 from setup import *
 from parallel import *
+from extract_audio import *
+
 
 def main():
     # defined text file name
@@ -8,6 +10,11 @@ def main():
 
     #Task 3
     parallel_download(filename, output_dir)
+
+    # Task4
+    video_folder= os.path.join(".", "parallel_download_outputs")
+    audio_folder= os.path.join(".", "audio_outputs")
+    audio_extraction(video_folder, audio_folder)
     
     
 if __name__=="__main__":
