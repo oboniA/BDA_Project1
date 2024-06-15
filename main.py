1288f370950ba09576d78869d3ad666eb1870ab2
Oboni <oanowe01@student.bbk.ac.uk>
2024-06-15 19:50:33 +0100

    Task 5 Translate text into spanish

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from parallel import *
 from extract_audio import *
 from transcribe_audio import *
 from analyse_sentiments import *
+from translate_text import *
 
 
 def main():
@@ -11,15 +12,16 @@ def main():
     filename='video_urls.txt'
 
     #Task 3
-    parallel_download(filename, output_dir)
+    #parallel_download(filename, output_dir)
 
     #Task 5
     video_folder= "./parallel_download_outputs"
     sub_directory = "./Task5_downloads"
 
-    audio_extraction(video_folder, sub_directory)
-    audio_transcription(sub_directory)
+    #audio_extraction(video_folder, sub_directory)
+    #audio_transcription(sub_directory)
     sentiment_analysis(sub_directory)
+    translate_to_spanish(sub_directory)
     
     
 if __name__=="__main__":
--- translate_text.py
+++ translate_text.py
@@ -1,38 +1,40 @@
 from textblob import TextBlob
 import os
 import threading
+from googletrans import Translator
 import time
 
 
-def text_analysis(speech_file, speech_location):
-    """sentiment analysis process from each transciption file"""
+def text_translation(speech_file, speech_location):
+    """Translation process for each transciption file to spanish"""
 
     try:
         # constructs text path from folder name and file name
         speech_path= os.path.join(speech_location, speech_file)
-    
+
         # read speech file containing audio transcription
         with open(speech_path, 'r') as sfile:
             text = sfile.read()
 
-        # sentiment extraction from transcription
+        # translation process
         blob = TextBlob(text)
-        sentiments = blob.sentiment
-        sentiment_text= f"'File': {speech_file}\n'Polarity': {sentiments.polarity}\n'Subjectivity': {sentiments.subjectivity}"
+        translator= Translator()
+        blob_translated=translator.translate(blob.raw, dest='es') # translated result
+        translated_str= str(blob_translated.text) # string format
 
         # constructs output file path
-        text_file = os.path.splitext(speech_file)[0] + '_sentiment_analysis.txt'
+        text_file = os.path.splitext(speech_file)[0] + '_Spanish.txt'
         out_path = os.path.join(speech_location, text_file)
-        with open(out_path, 'w') as f:
-            f.write(sentiment_text) # generates text file with analysis result
-        print(f'\nSentiment Analysis complete for {speech_file}; Worker Thread: {threading.current_thread().name}')
-
+        with open(out_path, 'w', encoding="utf-8") as f:
+            f.write(translated_str)
+        print(f'\nTranslation complete for {speech_file}; Worker Thread: {threading.current_thread().name}')
+            
     except Exception as e:
-        print(f'Error while sentiment analysis for {speech_file}: {e}')
-    
+        print(f'Error in Translating {speech_file}: {e}')
 
-def sentiment_analysis(speech_location):
-    """ Uses Threads for sentiment analysis from every transcription file"""
+
+def translate_to_spanish(speech_location):
+    """ Uses Threads to translate every transcripted file"""
 
     # list comprehension: generates list of all subdirectories in a specified directory
     subdirs= [os.path.join(speech_location, d) for d in os.listdir(speech_location) 
@@ -43,10 +45,10 @@ def sentiment_analysis(speech_location):
     for subdir in subdirs:
         text_files= [file for file in os.listdir(subdir) 
                      if file.endswith('.txt') 
-                     and '_sentiment_analysis.txt' not in file
+                     and '_sentiment_analysis' not in file
                      and '_Spanish.txt' not in file] 
         for text_file in text_files:
-            thread= threading.Thread(target=text_analysis, args=(text_file, subdir))
+            thread= threading.Thread(target=text_translation, args=(text_file, subdir))
             threads.append(thread)
             thread.start()
 
@@ -54,8 +56,9 @@ def sentiment_analysis(speech_location):
         thread.join()
 
     end=time.perf_counter()
-    print(f'Sentiment analysis finished in {end-start} seconds\n')
+    print(f'Translation finished in {end-start} seconds\n')
+
     
 
 
-    
+
